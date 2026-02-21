6055050e1647ec8c84ad3f734bbdd87a72dea22e
Aditya Kumar Nayak <adityakumarnayak77899@gmail.com>
2026-02-21 23:08:40 +0530

    Add files via upload

---
+++ Scatter plots.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x = [8,6,7,5,9,2]
+y=[8,3,7,6,9,4]
+plt.scatter(x,y,c="pink")
+plt. xlabel("DUDES")
+plt.ylabel("BADDIES")
+plt. title("THE COMPARISION")
+plt.show()
